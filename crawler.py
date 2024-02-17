6d0dbf6ef34e0ccbd8f7ced0eb6478c7df6d1210
SHA-15 <saleem.hkh@outlook.com>
2024-02-17 10:47:03 +0100

    Merginge Title, Location and Image-URL scraping in crawler.py

--- crawler.py
+++ crawler.py
@@ -6,9 +6,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
 import pandas as pd
 import numpy as np
+
+#Set Dataframe visibility
+pd.set_option("display.max_colwidth", None)
 
 # Set Chrome options
 options = Options()
@@ -41,17 +47,20 @@ print(f"Page title is: {driver.title}")
 
 # DATE, TIME, LOCATION
 information_array = []
+#TITLE & ARTIST
+title_artist_array = []
+#Image Links
+image_link_array = []
 i = 1
 while i >= 1:
     try:
-
         information_array.append(driver.find_element(By.XPATH, f'html/body/div[4]/main/section/ul/li[{i}]/div/div/div[2]/div[2]').text)
-
+        title_artist_array.append(driver.find_element(By.XPATH, f'/html/body/div[4]/main/section/ul/li[{i}]/div/div/div[2]/p/a').text)
+        image_link_array.append("https://www.lucernefestival.ch" + driver.find_element(By.XPATH, f'/html/body/div[4]/main/section/ul/li[{i}]/div/div/div[1]/a/figure/picture/source[1]').get_attribute('srcset'))
         # print(data_point)
         i += 1
-
     except:
-        # print("Function cycle ended at: %d, no more entries found" %i)
+        print("Function cycle ended at: %d, no more entries found" %i)
         break
 
 
@@ -64,38 +73,63 @@ for div in range(len(information_array)):
         information_array[div][sub_index] = information_array[div][sub_index].split("Program")[0].strip()
         information_array[div][sub_index] = information_array[div][sub_index].split("Summer")[0].strip()
 
-#print(information_array)
+#-----------------------------------DATE, TIME, LOCATION-----------------------------------------------------
+#Get Dates
+current_year = datetime.now().year
 
-#TITLE & ARTIST
-title_artist_array = []
-t = 1
+date_list = []
+for sub_list in range(len(information_array)):
+    date_string = information_array[sub_list][0]
+    date = datetime.strptime(date_string, "%a %d.%m.")
+    date = date.replace(year=current_year)
+    date = date.strftime("%d-%m-%Y")
+    date_list.append(date)
 
-while t >= 1:
-    try:
+#Get Time
+time_list = []
+for sub_time in range(len(information_array)):
+    time_string = information_array[sub_time][1]
+    time_format = datetime.strptime(time_string, "%H.%M")
+    time_format = time_format.strftime("%H:%M")
+    time_list.append(time_format)
 
-        title_artist_array.append(driver.find_element(By.XPATH, f'/html/body/div[4]/main/section/ul/li[{t}]/div/div/div[2]/p/a').text)
 
-        # print(f"Array iteration: {x}")
+#Get Locations
+locations_list = []
+for loc_index in range(len(information_array)):
+    event_location = information_array[loc_index][2]
+    if "Nr. 24" in event_location:
+        event_location = information_array[loc_index][3]
+        locations_list.append(event_location)
+    else:
+        locations_list.append(event_location)
 
-        t += 1
 
-    except:
-        break
+#Creating a DataFrame Object
+event_information_df = pd.DataFrame(
+    {
+        "Date": date_list,
+        "Time": time_list,
+        "Location": locations_list
+    }
+)
 
-#print(title_artist_array)
 
-#IMAGE LINK
-image_link_array = []
-l = 1
-while l >= 1:
-    try:
+print(event_information_df)
+#--------------------------------Event Title----------------------------------------------
+title_df = pd.DataFrame(
+    {
+        "Title": title_artist_array
+    }
+)
 
-        #Concatenating the HTTP address with the srcset attribute value to generate URL
-        image_link_array.append("https://www.lucernefestival.ch" + driver.find_element(By.XPATH, f'/html/body/div[4]/main/section/ul/li[{l}]/div/div/div[1]/a/figure/picture/source[1]').get_attribute('srcset'))
-        #print(f"Array iteration: {l}")
+print(title_df)
 
-        l += 1
-    except:
-        break
+#--------------------------------Image Links-----------------------------------------------
+image_df = pd.DataFrame(
+    {
+    "Image_URL": image_link_array
+    }
+)
 
-print(image_link_array)
+print(image_df)
